79ef7378ccb059d749b9a39030941f826e86ee53
porkostomus <porkostomus@mail.com>
2020-01-31 22:01:52 -0800

    render SVG

--- src/concepts/graph/web.cljs
+++ src/concepts/graph/web.cljs
@@ -1,11 +1,40 @@
 (ns concepts.graph.web
   (:require [reagent.core :as r]
-            [concepts.graph :as graph]
-            ["viz.js" :as viz]))
+            [concepts.graph :as graph]))
+
+(defn rect [w h x y]
+  [:rect
+   {:width        w
+    :height        h
+    :fill         "none"
+    :x            x
+    :y            y
+    :stroke       "#000000"
+    :stroke-width 1}])
+
+(defn text [s x y]
+  [:text {:x           x
+          :y           y
+          :text-anchor "middle"
+          :font-size   11}
+   s])
+
+(defn node [s x y]
+  [:g 
+   [rect (* (count s) 7) 11 x y]
+   [text s (+ 13 x) (+ 9 y)]])
+
+(defn edge [x1 y1 x2 y2]
+  [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2
+          :stroke "black"}])
 
 (defn app []
   [:div#app
-   [:h1 "concepts.graph.web"]])
+   [:h1 "concepts.graph.web"]
+   [:svg {:width    "100%"
+          :view-box (str "0 0 100 100")}
+    [node "Root" 30 5]
+    [edge 45 16 45 40]]])
 
 (defn render []
   (r/render [app]
@@ -16,7 +45,6 @@
   (js/console.log "start"))
 
 (defn ^:export init []
-  (js/console.log "init")
   (start))
 
 (defn ^:dev/before-load stop []
